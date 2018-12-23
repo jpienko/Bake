067e6c7c3c028f0b819b47ab336b782c7b0f6feb
jpienko <j.pienko@onet.pl>
2018-12-23 23:57:15 +0100

    part of visual ready

--- cake-shop/src/app/add-cake/add-cake.component.ts
+++ cake-shop/src/app/add-cake/add-cake.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,Inject } from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import { FormBuilder, Validators, FormGroup} from  '@angular/forms';
+import {Cakes} from "../main-list/main-list.component"
 
 
 @Component({
@@ -9,13 +10,28 @@ import { FormBuilder, Validators, FormGroup} from  '@angular/forms';
   styleUrls: ['./add-cake.component.scss']
 })
 export class AddCakeComponent implements OnInit {
-    constructor() {}
-    ngOnInit(){}
+
+  protected cakePic:string="assets/imgs/CakeShop.png";
+
+  protected model = new Cakes;
+  protected cake: FormGroup;
+  piecePr:string = "0 PLN";
+
+  constructor(private formBuilder: FormBuilder,) {
+    this.cake = this.formBuilder.group({
+      pic:[''],
+      name: [''],
+      description: [''],
+      price:[''],
+      pieces:[''],
+      piecePrice:['']
+    });
+  }
+  ngOnInit(){}
       
+  protected save(){
+
+  }
 
 
 }
-export interface DialogData {
-    animal: string;
-    name: string;
-  }
--- cake-shop/src/app/app.module.ts
+++ cake-shop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {  MatDialogModule, MatListModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule } from '@angular/material';
+import {  MatDialogModule, MatListModule, MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatRippleModule, MatGridListModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,7 +8,7 @@ import { MainListComponent } from './main-list/main-list.component';
 import { FormsModule, ReactiveFormsModule } from '../../node_modules/@angular/forms';
 import { AddCakeComponent } from './add-cake/add-cake.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { InputCounterModule } from 'ng4-input-counter';
 
 @NgModule({
   declarations: [
@@ -29,7 +29,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatFormFieldModule,
     MatInputModule,
     MatRippleModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    InputCounterModule.forRoot(),
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent],
